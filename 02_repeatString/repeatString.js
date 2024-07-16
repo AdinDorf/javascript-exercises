db8464c8d692b2fd8d7205be5f282524d26a7850
AdinDorf <adin@dorffamily.com>
2024-07-16 12:20:53 -0500

    Completed repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let retString = '';
+    if (num < 0){
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++)
+    {
+        retString += str;
+    }
+    return retString;
 };
 
 // Do not edit below this line
