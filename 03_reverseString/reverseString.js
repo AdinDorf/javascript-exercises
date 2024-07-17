90338af867d35ce3b73825c9c76757aebcd4bdac
AdinDorf <adin@dorffamily.com>
2024-07-16 23:56:17 -0500

    Completed reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (startingString) {
+    let str = '';
+    for (let i = startingString.length-1; i >= 0; i--){
+        str += startingString.charAt(i);
+    }
+    return str;
 };
 
 // Do not edit below this line
