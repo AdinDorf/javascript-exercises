b8f4f37f8a2ae587de22e0f29360b09899cf45f5
AdinDorf <adin@dorffamily.com>
2024-07-17 13:41:48 -0500

    Completed removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,13 @@
-const removeFromArray = function(arr, remove) {
-    return arr.filter((element) => {
-       return element != remove;
+const removeFromArray = function(arr, ...toRemoveArr) {
+    let newArr = arr;
+    toRemoveArr.forEach(toRemove => {
+        console.log("Removing " + toRemove);
+        newArr = newArr.filter((element) => {
+            return element !== toRemove;
+         });
     });
+    console.log("returning");
+    return newArr
 };
 
 // Do not edit below this line
