cc2cc5a0be6fbc92dfef7027d808824c6a38e608
AdinDorf <adin@dorffamily.com>
2024-07-25 11:53:48 -0500

    Completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,18 @@
-const palindromes = function () {
+const palindromes = function (originalString) {
+
+let cleanedString = originalString.split('').filter(
+    (char) => {
+        return /[a-zA-Z0-9]/g.test(char);
+    }
+).join('').toLowerCase();
+
+let reversedString = cleanedString.split('').reduce(
+    (accumulator, current) =>  current + accumulator, 
+);
+
+if (reversedString === cleanedString) return true;
+
+return false;
 
 };
 
