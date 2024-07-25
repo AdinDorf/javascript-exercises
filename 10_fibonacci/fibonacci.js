5b51fbdfb59d88b1f0ad227c9915025825f3010a
AdinDorf <adin@dorffamily.com>
2024-07-25 12:07:31 -0500

    Completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    if (num < 0)
+        return 'OOPS';
+
+    if (num == 0)
+        return 0;
+
+    if (num == 1)
+        return 1;
+
+    if (num == 2)
+        return 1;
+
+    return fibonacci(num-2) + fibonacci(num-1);
 
 };
 
