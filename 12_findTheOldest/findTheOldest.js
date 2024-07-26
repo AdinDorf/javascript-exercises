2660286da1d39f8e44f6574ba0232e4323a8150e
AdinDorf <adin@dorffamily.com>
2024-07-26 16:38:32 -0500

    Completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,22 @@
-const findTheOldest = function() {
+const date = new Date().getFullYear();
+const findTheOldest = function(people) {
+    
+function getAge(person) {
+    if (!person.yearOfDeath)
+        return date - person.yearOfBirth;
+    return person.yearOfDeath - person.yearOfBirth
+};
+
+return people.reduce((oldest, current) => {
+    let oldestAge = getAge(oldest);
+    let currentAge = getAge(current);
+
+    if (oldestAge < currentAge)
+    {
+        return current
+    }
+    return oldest
+});
 
 };
 
